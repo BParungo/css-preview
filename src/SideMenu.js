86d250273db2d4298423f539500cdb62edc50a8c
bparu <bparungo@web.de>
2022-04-14 22:21:49 +0200

    testing sliders

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,13 +1,14 @@
-
+import React, { useState } from 'react';
 function SideMenu() {
+    const [size,setSize] = useState(18);
   return (
     <div className="App">
-      <label htmlFor="fontsize">Volume</label>
-      <input type="range" id="fontsize" name="fontsize"
-             min="1" max="100"/>
-      <p style={{fontSize: px}}>asdfgwehewhs</p>
+      <label htmlFor="fontsize">{size}</label>
+      <input type="range" id="fontsize" name="fontsize" onChange={(evt) => setSize(+evt.target.value) }
+             min="1" max="500"/>
+      <p style={{fontSize: size}}>asdfgwehewhs</p>
+        <input type="color"/>
     </div>
-
   );
 }
 
